4551ab6a2e6479757cd8befc7de81a3881c13e9a
RONG CAI <micarfield@gmail.com>
2019-06-21 19:12:00 +0800

    background pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to Design and Code React apps</h1>
-    <p>complete courses about hte best tools and design systems. Prototype and buold apps with React and Swift.</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+       <h1>Learn to Design and Code React apps</h1>
+       <p>complete courses about hte best tools and design systems. Prototype and buold apps with React and Swift.</p>
+       <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
+
   </div>
 )
 
