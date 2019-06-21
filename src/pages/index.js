497c4ba23e7a4822a0a1d17f6f2efdf45e4f231a
RONG CAI <micarfield@gmail.com>
2019-06-21 18:07:10 +0800

    watch button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Learn to Design and Code React apps</h1>
+    <p>complete courses about hte best tools and design systems. Prototype and buold apps with React and Swift.</p>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
